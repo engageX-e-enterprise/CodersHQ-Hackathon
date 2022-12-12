6ab6441de34988e616beec0957909f26080e8a10
mohamed-gwad <114035318+mohamed-gwad@users.noreply.github.com>
2022-12-12 19:22:07 +0400

    Add files via upload

---
+++ Workshop-WhatsApp.py
@@ -0,0 +1,21 @@
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ACCOUNT_SID = "AC4677df5a4b92eb6ca8a7705138715e28"
+AUTH_TOKEN = os.getenv('AUTH_TOKEN')
+PHONE_NUMBNER = os.getenv('PHONE_NUMBNER')
+
+
+
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+message = client.messages.create(
+    body='Hi there',
+    from_='whatsapp:+14155238886',
+    to='whatsapp:+971508440128'
+)
+
+print(message.sid)
